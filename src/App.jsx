0d50147427813b913820d6cd76c7430c221c99ec
Sujit <pandasujit687@gmail.com>
2025-03-30 00:07:40 +0530

    show footer add

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
 
   const addValue = () => {
-    setCounter(counter + 1)
-  }
+    setCounter(counter + 1);
+  };
 
   const removeValue = () => {
-    setCounter(counter -1)
-  }
- 
+    setCounter(counter - 1);
+  };
+
   return (
     <>
-    <h1>Counter</h1>
+      <h1>Counter</h1>
       <h2>Counter value: {counter}</h2>
-      <button 
-      onClick={addValue}>Add value</button>
+      <button onClick={addValue}>Add value</button>
       <br />
       <button onClick={removeValue}>Remove Value</button>
       <p>footer: {counter}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
